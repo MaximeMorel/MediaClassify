017990354fc0ca5121118a6a56709ef8bdcc4fc1
MaximeMorel <maxime.morel69@gmail.com>
2016-11-23 20:34:29 +0000

    Stats, copy failed files, do not overwrite existing files

--- media-classify.py
+++ media-classify.py
@@ -11,11 +11,18 @@
 # 1 - year/year_month/
 # 2 - year/year_month_day/
 
+# Changelog
+# 2016/11/23
+# Show some stats at the end of execution
+# Do not overwrite existing files in destination folder, but append a number to the file
+# Copy failed files (metadata could not be found) to destination under fail folder
+
 import os
 import sys
+import string
 import datetime
 import shutil
-from gi.repository import GExiv2
+#from gi.repository import GExiv2
 import pyexiv2
 from os.path import join, normpath
 
@@ -24,6 +31,9 @@ class Classifier:
 		self.source = 'source'
 		self.destination = 'destination'
 		self.mode = 1
+		self.numClassifed = 0
+		self.numFailed = 0
+		self.numIgnored = 0
 
 	def setParams(self, argv):
 		if len(argv) >= 2:
@@ -34,7 +44,6 @@ class Classifier:
 			self.mode = int(argv[3])
 
 	def getMetaData(self, filename):
-		#print(filename)
 		try:
 			#metadata = GExiv2.Metadata(filename)
 			#print(metadata)
@@ -50,18 +59,34 @@ class Classifier:
 			pass
 		return datetime.datetime(1, 1, 1, 0, 0, 0)
 
-	def mkDestDir(self, date):
+	def genDestDirName(self, date):
 		dest = join(self.destination, str(date.year))
 		if self.mode >= 1:
-			dest = join(dest, str(date.year) + '_' + str(date.month))
+			dest = join(dest, str(date.year) + '_' + str(date.month).zfill(2))
 		if self.mode >= 2:
-			dest += '_' + str(date.day)
+			dest += '_' + str(date.day).zfill(2)
+		return dest
+
+	def copy(self, src, dstdir, dstfile):
 		try:
-			os.makedirs(dest)
-			print('mkdir: ' + dest)
+			os.makedirs(dstdir)
+			print('mkdir: ' + dstdir)
 		except:
 			pass
-		return dest
+		dest = join(dstdir, dstfile)
+		dst = dest
+		pos = string.rfind(dest, '.')
+		i = 0
+		if pos < 0:
+			print('bad src: ' + src)
+		else:
+			while os.path.isfile(dest):
+				# destination already exists, generate new name
+				i += 1
+				dest = dst[:pos] + '_' + str(i) + dst[pos:]
+			shutil.copy2(src, dest)
+			if i > 0:
+				print('  renamed: ' + dest)
 
 	def run(self):
 		print('Working directory: ' + os.getcwd())
@@ -82,22 +107,32 @@ class Classifier:
 				basefile, extension = os.path.splitext(file)
 				extension = extension.lower()
 				srcfile = join(root, file)
-				#if extension in ['.jpg', '.jpeg', '.bmp', '.png', '.cr2']:
 				if extension in ['.jpg', '.jpeg', '.bmp', '.png', '.cr2', '.thm', '.mov', '.mp4', '.wmv', '.mod', '.mpg', '.avi', '.mts', '.mkv', ]:
 					t = self.getMetaData(srcfile)
-					if t.year != 1:
-						dest = self.mkDestDir(t)
+					if isinstance(t, datetime.datetime) and t.year != 1:
+						dest = self.genDestDirName(t)
+						self.copy(srcfile, dest, file)
 						print('copy: ' + srcfile + ' -> ' + join(dest, file))
-						shutil.copy2(srcfile, dest)
+						self.numClassifed += 1
 					else:
-						print('fail: ' + srcfile + ' -> ' + join(dest, file))
+						faildest = join(self.destination, 'fail', root[len(self.source):])
+						self.copy(srcfile, faildest, file)
+						print('fail: ' + srcfile + ' -> ' + join(faildest, file))
+						self.numFailed += 1
 				else:
 					print('unknown: ' + srcfile)
+					self.numIgnored += 1
 				sys.stdout.flush()
+
+	def showStats(self):
+		print('Num classified: ' + str(self.numClassifed))
+		print('Num failed: ' + str(self.numFailed))
+		print('Num ignored: ' + str(self.numIgnored))
 
 if __name__ == '__main__':
 	print('Picture and video classification script')
 	classifier = Classifier()
 	classifier.setParams(sys.argv)
 	classifier.run()
+	classifier.showStats()
 
